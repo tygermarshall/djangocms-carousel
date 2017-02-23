28cdf548ab75978649877781996cfc83f2774f20
René Fleschenberg <rene@fleschenberg.net>
2017-02-23 20:39:57 +0100

    Bump version: 0.2.2 → 0.3.0

--- setup.py
+++ setup.py
@@ -24,7 +24,7 @@ def read(*names, **kwargs):
 
 setup(
     name='awesto-djangocms-carousel',
-    version='0.2.2',
+    version='0.3.0',
     license='MIT',
     description='A carousel plugin for djangoCMS',
     long_description='%s\n%s' % (
