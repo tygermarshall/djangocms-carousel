f2d97ebb74dae6997f138ea77aded2715277d30b
René Fleschenberg <rene@fleschenberg.net>
2017-02-23 12:15:54 +0100

    Test settings

--- src/cmsplugin_carousel/settings_test.py
+++ src/cmsplugin_carousel/settings_test.py
@@ -1,4 +1,47 @@
+SECRET_KEY = 'notsecret'
+
 INSTALLED_APPS = [
+    'django.contrib.admin',
+    'django.contrib.auth',
+    'django.contrib.contenttypes',
+    'django.contrib.sessions',
+    'django.contrib.messages',
+    'django.contrib.staticfiles',
+    'django.contrib.sites',
+    'cms',
+    'treebeard',
+    'menus',
+    'easy_thumbnails',
+    'filer',
     'adminsortable',
     'cmsplugin_carousel',
+]
+
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+    }
+}
+
+SITE_ID = 1
+LANGUAGES = [
+    ('en-us', 'English'),
+]
+
+TEMPLATES = [
+    {
+        'BACKEND': 'django.template.backends.django.DjangoTemplates',
+        'DIRS': [],
+        'APP_DIRS': True,
+        'OPTIONS': {
+            'context_processors': [
+                'django.template.context_processors.debug',
+                'django.template.context_processors.request',
+                'django.contrib.auth.context_processors.auth',
+                'django.contrib.messages.context_processors.messages',
+                'sekizai.context_processors.sekizai',
+                'cms.context_processors.cms_settings',
+            ],
+        },
+    },
 ]
